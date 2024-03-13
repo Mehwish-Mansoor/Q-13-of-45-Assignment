122353bcbaca332df98df14330b5db283f4ce17b
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-13 11:45:10 +0500

    Add files via upload

---
+++ qno13.js
@@ -0,0 +1,8 @@
+"use strict";
+//  13. your own Array: think of yoyr favourit mod of transportation,
+//  such as a motorcycle or a car, and make a list that stores several examples.
+//  use your list to print a series of statements about these item, such as
+//   "i would like to own a honda motorcycle."
+Object.defineProperty(exports, "__esModule", { value: true });
+let vehichles = ["Honda", "Honda city", "Rivo", "Hilux"];
+vehichles.map((items) => console.log(`i would like to own a ${items}`));
---
+++ qno13.ts
@@ -0,0 +1,15 @@
+//  13. your own Array: think of yoyr favourit mod of transportation,
+//  such as a motorcycle or a car, and make a list that stores several examples.
+//  use your list to print a series of statements about these item, such as
+//   "i would like to own a honda motorcycle."
+
+
+let vehichles: string[] = ["Honda", "Honda city", "Rivo", "Hilux"]
+
+vehichles.map((items) => console.log( `i would like to own a ${items}`));
+
+
+
+
+
+
